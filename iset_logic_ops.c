3e9fd0670e2e3d5732dc6fc9ef3e542ef421181e
Christian Solomon <cncs.chris@gmail.com>
2023-06-21 10:26:19 +0000

    update -> logic_ops[config@init >> config@betty]

---
+++ iset_logic_ops.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * m_add - adds the first two items in a stack
+ *
+ * @h: head pointer to the stack
+ * @ln: line number
+ *
+ * Return: void
+*/
+void m_add(stack_t **h, unsigned int ln)
+{
+	printf("add -> m_add has been called");
+}
+
+/**
+ * m_sub - subtracts the first two items in a stack
+ *
+ * @h: head pointer to the stack
+ * @ln: line number
+ *
+ * Return: void
+*/
+void m_sub(stack_t **h, unsigned int ln)
+{
+	printf("add -> m_add has been called");
+}
+
+/**
+ * m_div - divides the first two items in a stack
+ *
+ * @h: head pointer to the stack
+ * @ln: line number
+ *
+ * Return: void
+*/
+void m_div(stack_t **h, unsigned int ln)
+{
+	printf("add -> m_add has been called");
+}
+
+/**
+ * m_mul - multiplies the first two items in a stack
+ *
+ * @h: head pointer to the stack
+ * @ln: line number
+ *
+ * Return: void
+*/
+void m_mul(stack_t **h, unsigned int ln)
+{
+	printf("add -> m_add has been called");
+}
+
+/**
+ * m_mod - finds the modulo of the first two items in a stack
+ *
+ * @h: head pointer to the stack
+ * @ln: line number
+ *
+ * Return: void
+*/
+void m_mod(stack_t **h, unsigned int ln)
+{
+	printf("add -> m_add has been called");
+}
